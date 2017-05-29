28c19715081886ea0c257eb78eb03c06d70d2a1b
gopenshaw <garrett.j.openshaw@gmail.com>
2017-05-29 10:03:27 -0400

    Get all ASNs and write to file

--- asn_scraper.py
+++ asn_scraper.py
@@ -1,8 +1,6 @@
-import urllib2
 import bs4
-
-def fun():
-    print('hey')
+import json
+import urllib2
 
 def url_to_soup(url):
     # bgp.he.net filters based on user-agent.
@@ -10,6 +8,26 @@ def url_to_soup(url):
     html = urllib2.urlopen(req).read()
     soup = bs4.BeautifulSoup(html)
     return soup
+
+def get_all_asns(outfile=None):
+    '''
+    Return a dictionary with info for each ASN
+    Key is ASN number
+    Value is a dictionary containing information about the ASN, for example
+        {'Country': 'US',
+        'Name': 'University of Michigan',
+        'Routes v4': 14,
+        'Routes v6': 1}}
+    '''
+    asns = dict()
+    for c in get_asn_country_urls():
+        asns.update(get_asns_from_country(c[0], c[1]))
+
+    if outfile:
+        with open(outfile, 'w') as f:
+            json.dump(asns, f, indent=4, sort_keys=True)
+
+    return asns
 
 def _get_country_url(row, base_url):
     ''' Return (url, country_code) tuple from a table row '''
@@ -37,21 +55,28 @@ def get_asn_country_urls():
 def get_asns_from_country(asn_country_url, country_code):
     asn_country = url_to_soup(asn_country_url)
     asn_table = asn_country.findAll('table')
+    if not asn_table:
+        # some countries have no actives ASNS
+        return []
+
     if len(asn_table) > 1:
-        raise RuntimeError('More than one table at {}'.format(
-            asn_country_url))
+        raise RuntimeError(('Unexpected content at {}. More than one '
+            'table found.').format(asn_country_url))
     asn_tr_tags = asn_table[0].findAll('tr')
     asn_tr_tags = asn_tr_tags[1:] # remove headers
     # TODO get real country code
     asns = [_build_asn_from_tag(a, country_code) for a in asn_tr_tags]
     return asns
 
+def str_to_int(s):
+    return int(s.replace(',', ''))
+
 def _build_asn_from_tag(asn_tr_tag, country_code):
     tds = asn_tr_tag.findAll('td')
-    asn_number = tds[0].getText().strip('AS')
+    asn_number = str_to_int(tds[0].getText().strip('AS'))
     name = tds[1].getText()
-    v4_routes = tds[3].getText()
-    v6_routes = tds[5].getText()
+    v4_routes = str_to_int(tds[3].getText())
+    v6_routes = str_to_int(tds[5].getText())
     return (asn_number, {'Country': country_code,
         'Name': name, 'Routes v4': v4_routes, 'Routes v6': v6_routes})
     
